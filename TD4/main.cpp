5cacde3c53ce0f49981eff09b79647496b181516
MrBigoudi <yannis.kedadry2@gmail.com>
2023-10-27 20:53:55 +0200

    trying to fix stuff

--- TD4/main.cpp
+++ TD4/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <igl/opengl/glfw/Viewer.h>
 #include <iostream>
 #include <ostream>
@@ -13,17 +14,17 @@
 #include <igl/gaussian_curvature.h>
 #include <igl/per_vertex_normals.h>
 #include <igl/per_face_normals.h>
+#include <vector>
 
 #include "HalfedgeBuilder.cpp"
-
+#include "igl/PI.h"
+#include "igl/cross.h"
 
 using namespace Eigen; // to use the classes provided by Eigen library
 using namespace std;
 
 MatrixXd V;
 MatrixXi F;
-
-
 
 
 MatrixXd N_faces;   //computed calling pre-defined functions of LibiGL
@@ -40,6 +41,13 @@ MatrixXd lib_curvatures; // computed using the face-vertex structure of LibiGL
 
 MatrixXd voronoi; // area of the dual voronoi areas of every vertex
 
+
+// colors for the dfs
+enum COLORS{
+	WHITE,
+	BLACK,
+	GREY,
+};
 
 SparseMatrix<double> L; //cotangent
 SparseMatrix<double> A; //normalization for the laplacian
@@ -66,6 +74,10 @@ bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier
 	return false;
 }
 
+double cotangent(double x){
+	return 1/(tan(x));
+}
+
 namespace mesh_methods{
 	
 	void updateColor(MatrixXd &C, double t = 0.0){
@@ -81,11 +93,11 @@ namespace mesh_methods{
 		// TO BE COMPLETED
 		int res = 0;
 		int beg = he.getEdge(v);
-		int nEdge = he.getOpposite(he.getNext(beg));
-		while(nEdge != beg){
-			nEdge = he.getOpposite(he.getNext(nEdge));
+		int next = beg;
+		do{
+			next = he.getOpposite(he.getNext(next));
 			res++;
-		}
+		}while(next != beg);
 		return res;
 	}
 
@@ -93,11 +105,9 @@ namespace mesh_methods{
 	 * Return the degree of a given vertex 'v'
 	 * Turn around vertex 'v' in CCW order
 	 **/
-	int vertexDegreeCCW(HalfedgeDS he, int v)
-	{
-	        // TO BE COMPLETED
-	        
-	        return 0;
+	int vertexDegreeCCW(HalfedgeDS he, int v){
+		// TO BE COMPLETED
+		return 0;
 	}
 
 	/**
@@ -106,7 +116,37 @@ namespace mesh_methods{
 	 **/
 	void vertexNormals(HalfedgeDS he) {
 		// TODO --fill the matrix he_N_vertices
-		
+		int nbVert = he.sizeOfVertices();
+		he_N_vertices = MatrixXd::Zero(nbVert, 3);
+		for(int v=0; v<nbVert; v++){
+			MatrixXd normal = MatrixXd::Zero(1,3);
+			int beg = he.getEdge(v);
+			int prev = beg;
+			int next = he.getOpposite(he.getNext(prev));
+			// find all the faces surrounding it
+			do{
+				// get the face normal
+				MatrixXd e1 = MatrixXd::Zero(1,3);
+				MatrixXd e2 = MatrixXd::Zero(1,3);
+				int v1 = he.getTarget(he.getOpposite(prev));
+				int v2 = he.getTarget(he.getOpposite(next));
+
+				int tmp = v;
+				tmp = min(tmp, v1);
+
+
+				e1.row(0) = V.row(v1) - V.row(v);
+				e2.row(0) = V.row(v2) - V.row(v);
+				MatrixXd normalTmp = MatrixXd::Zero(1,3);
+				igl::cross(e2, e1, normalTmp);
+				normalTmp.normalize();
+				normal += normalTmp;
+
+				prev = next;
+				next = he.getOpposite(he.getNext(next));
+			}while(prev != beg);
+			he_N_vertices.row(v) = normal.normalized();
+		}
 	}
 
 	// Compute lib_per-vertex normals
@@ -116,6 +156,33 @@ namespace mesh_methods{
 	**/
 	void lib_vertexNormals() {
 		// TODO -- fill the matrix lib_N_vertices
+		lib_N_vertices = MatrixXd::Zero(V.rows(), 3);
+		// get the faces normals
+		for(int f=0; f<F.rows(); f++){
+			// get the vertices
+			int v1 = F.row(f).x();
+			int v2 = F.row(f).y();
+			int v3 = F.row(f).z();
+
+			// get the face normal
+			MatrixXd e1 = MatrixXd::Zero(1,3);
+			MatrixXd e2 = MatrixXd::Zero(1,3);
+			e1.row(0) = V.row(v3) - V.row(v2);
+			e2.row(0) = V.row(v1) - V.row(v2);
+			MatrixXd normal = MatrixXd::Zero(1,3);
+			igl::cross(e1, e2, normal);
+			normal.normalize();
+
+			// add the normal to the vertices normals
+			lib_N_vertices.row(v1) += normal;
+			lib_N_vertices.row(v2) += normal;
+			lib_N_vertices.row(v3) += normal;
+		}
+
+		// normalize the vertices normals
+		for(int v=0; v<V.rows(); v++){
+			lib_N_vertices.row(v).normalize();
+		}
 	}
 
 	/**
@@ -154,12 +221,57 @@ namespace mesh_methods{
 		//TODO -- fill the matrix lib_Deg_vertices
 		std::cout << "Computing vertex degrees using libigl...";
 		auto start = std::chrono::high_resolution_clock::now(); // for measuring time performances
-		lib_Deg_vertices = MatrixXi(V.rows(),1);
-		for(int i=0; i<V.rows(); i++){
-			lib_Deg_vertices(i,0) = V.row(i).size();
+		lib_Deg_vertices = MatrixXi::Zero(V.rows(),1);
+
+		for(int f=0; f<F.rows(); f++){
+			for(int v=0; v<F.row(f).size(); v++){
+				lib_Deg_vertices(F.row(f)[v], 0)++;
+				// lib_Deg_vertices(v1,0)+=2;
+				// lib_Deg_vertices(v2,0)+=2;
+				// lib_Deg_vertices(v3,0)+=2;
+			}
 		}
+		// do not need to divide by 2 if only adding 1
+		// for(int v=0; v<V.rows(); v++){
+		// 	lib_Deg_vertices(v,0) /= 2;
+		// }
 		auto end = std::chrono::high_resolution_clock::now(); // for measuring time performances
 		std::cout << "Time spent: " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << "ms" << std::endl;
+	}
+
+	/**
+	 * Get all the neighbours of a given vertex
+	 * @param he The half edge structure
+	 * @param v The current vertex
+	 * @return All its neighbours as a vector
+	*/
+	std::vector<int> getAllVerticesNeighbours(HalfedgeDS& he, int v){
+		std::vector<int> neighbours = std::vector<int>();
+		int beg = he.getEdge(v);
+		int next = beg;
+		do{
+			neighbours.push_back(he.getTarget(he.getOpposite(next)));
+			next = he.getOpposite(he.getNext(next));
+		}while(next != beg);
+
+		return neighbours;
+	}
+
+	/**
+	 * Do an iteration of the DFS visit
+	 * @param he The half edge structure
+	 * @param v The current vertex
+	 * @param visited The state of all the vertices
+	*/
+	void dfsVisit(HalfedgeDS& he, int v, MatrixXi& visited){
+		visited.row(v)[0] = GREY;
+		std::vector<int> neighbours = getAllVerticesNeighbours(he, v);
+		for(int u : neighbours){
+			if(visited.row(u)[0] == WHITE){
+				dfsVisit(he, u, visited);
+			}
+		}
+		visited.row(v)[0] = BLACK;
 	}
     	
 	/**
@@ -168,7 +280,44 @@ namespace mesh_methods{
 	**/
 	int countBoundaries(HalfedgeDS he){
 		// TO BE COMPLETED
-		return 0;
+
+		int nbConnectedComponents = 0;
+		MatrixXi visited = MatrixXi::Zero(V.rows(),1);
+		for(int v=0; v<V.rows(); v++){
+			visited.row(v)[0] = WHITE;
+		}
+
+		// do a DFS
+		for(int v=0; v<V.rows(); v++){
+			if(visited.row(v)[0] == WHITE){
+				nbConnectedComponents++;
+				dfsVisit(he, v, visited);
+			}
+		}
+
+		return nbConnectedComponents;
+	}
+
+	/**
+	 * Given the index of 3 vertices, get the angle between them
+	 * @param vIdx The vertex origin of the angle
+	 * @param v1Idx The first vertex
+	 * @param v2Idx The second vertex
+	 * @return The angle between the two vertices
+	*/
+	float getTipAngle(int vIdx, int v1Idx, int v2Idx){
+		VectorXd v  = V.row(vIdx);
+		VectorXd v1 = V.row(v1Idx);
+		VectorXd v2 = V.row(v2Idx);
+
+		v1 -= v;
+		v2 -= v;
+
+		float dot = v1.x()*v2.x() + v1.y()*v2.y() + v1.z()*v2.z();
+
+		float res = acos(dot / (v1.norm()*v2.norm()));
+
+		return res;
 	}
 
 	/**
@@ -178,15 +327,84 @@ namespace mesh_methods{
 	 */
 	void compute_voronoi_area(){
 		//TODO
+		voronoi = MatrixXd::Zero(V.rows(),1);
+		for(int f=0; f<F.rows(); f++){
+			int v1Idx = F.row(f)[0];
+			int v2Idx = F.row(f)[1];
+			int v3Idx = F.row(f)[2];
+
+			VectorXd v1 = V.row(v1Idx);
+			VectorXd v2 = V.row(v2Idx);
+			VectorXd v3 = V.row(v3Idx);
+
+			float dist_12 = (v1-v2).norm()*(v1-v2).norm();
+			float dist_23 = (v2-v3).norm()*(v2-v3).norm();
+			float dist_31 = (v3-v1).norm()*(v3-v1).norm();
+
+			float alpha_1 = getTipAngle(v1Idx, v2Idx, v3Idx);
+			float alpha_2 = getTipAngle(v2Idx, v3Idx, v1Idx);
+			float alpha_3 = getTipAngle(v3Idx, v1Idx, v2Idx);
+
+			if(alpha_1 < igl::PI / 2.0f){
+				voronoi.row(v2Idx)[0] += cotangent(alpha_1)*dist_23;
+				voronoi.row(v3Idx)[0] += cotangent(alpha_1)*dist_23;
+			}
+
+			if(alpha_2 < igl::PI / 2.0f){
+				voronoi.row(v1Idx)[0] += cotangent(alpha_2)*dist_31;
+				voronoi.row(v3Idx)[0] += cotangent(alpha_2)*dist_31;
+			}
+
+			if(alpha_3 < igl::PI / 2.0f){
+				voronoi.row(v1Idx)[0] += cotangent(alpha_3)*dist_12;
+				voronoi.row(v2Idx)[0] += cotangent(alpha_3)*dist_12;
+			}
+		}
+
+		for(int v=0; v<V.rows(); v++){
+			voronoi.row(v)[0] /= 8.;
+			std::cout << "Voronoi[" << v << "] = " << voronoi.row(v)[0] << std::endl;
+		}
 	}
 
 	void compute_gaussian_curvature(HalfedgeDS he){
 		// TODO
-
+		curvatures = MatrixXd::Zero(V.rows(),1);
+		// for each vertex, get the sumof tip angles
+		for(int v=0; v<V.rows(); v++){
+			float sum = 0.0f;
+			std::vector<int> neighbours = getAllVerticesNeighbours(he, v);
+			for(int i=0; i<neighbours.size(); i++){
+				int prev = neighbours[i];
+				int next = neighbours[(i+1)%neighbours.size()];
+				sum += getTipAngle(v, prev, next);
+			}
+			// get the gaussian curvature
+			curvatures.row(v)[0] = (2*igl::PI - sum) / voronoi.row(v)[0];
+		}
 	}
 
 	void compute_lib_gaussian_curvature(){
 		// TODO
+		lib_curvatures = MatrixXd::Zero(V.rows(),1);
+		// for each face, get the sumof tip angles
+		for(int f=0; f<F.rows(); f++){
+			int v1Idx = F.row(f)[0];
+			int v2Idx = F.row(f)[1];
+			int v3Idx = F.row(f)[2];
+
+			VectorXd v1 = V.row(v1Idx);
+			VectorXd v2 = V.row(v2Idx);
+			VectorXd v3 = V.row(v3Idx);
+
+			lib_curvatures.row(v1Idx)[0] += getTipAngle(v1Idx, v2Idx, v3Idx);
+			lib_curvatures.row(v2Idx)[0] += getTipAngle(v2Idx, v3Idx, v1Idx);
+			lib_curvatures.row(v3Idx)[0] += getTipAngle(v3Idx, v1Idx, v2Idx);
+		}
+		// rearrange the curvature for every vertex
+		for(int v=0; v<V.rows(); v++){
+			lib_curvatures.row(v)[0] = (2*igl::PI - lib_curvatures.row(v)[0]) / voronoi.row(v)[0];
+		}
 	}
 
 	void updateColorGaussianCurvature(MatrixXd &C, double t = 0.0){
@@ -197,10 +415,6 @@ namespace mesh_methods{
 
 
 namespace laplacian{
-
-	double cotangent(double x){
-		return 1/(tan(x));
-	}
 
 	void buildLaplacian(const MatrixXd &V, const MatrixXi &F, SparseMatrix<double> &L, SparseMatrix<double> &A, SparseMatrix<double> &Ainv, SparseMatrix<double> &Delta){
 
@@ -267,7 +481,24 @@ namespace laplacian{
 // ------------ main program ----------------
 int main(int argc, char *argv[]) {
 
-    igl::readOFF("./data/cat0.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/bunny_coarser_2.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/bunny_coarser.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/bunny_cut.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/bunny_fine.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/bunny_new.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/bunny.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/cat0.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/chandelier.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/cube_open.off",V,F); // change this line depending on your system
+    igl::readOFF("./data/cube_tri.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/face.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/high_genus.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/homer.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/nefertiti.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/sphere.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/star.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/torus.off",V,F); // change this line depending on your system
+    // igl::readOFF("./data/venus.off",V,F); // change this line depending on your system
 
 	//print the number of mesh elements
     std::cout << "Points: " << V.rows() << std::endl;
@@ -301,6 +532,13 @@ int main(int argc, char *argv[]) {
 	// compute vertex degrees
 	mesh_methods::vertexDegreeStatistics(he); 
 	mesh_methods::lib_vertexDegrees();
+	// for(int v=0; v<V.rows(); v++){
+	// 	int deg1 = mesh_methods::vertexDegree(he, v);
+	// 	int deg2 = lib_Deg_vertices.row(v).x();
+	// 	if(deg1 != deg2){
+	// 		std::cout << "wrong output: " << deg1 << " != " << deg2 << std::endl;
+	// 	}
+	// }
 
 	// TASK 1.3
 	// Compute normals
@@ -312,15 +550,22 @@ int main(int argc, char *argv[]) {
 	igl::per_vertex_normals(V,F,N_vertices);
 	
 	// Compute lib_per-vertex normals
-	// mesh_methods::lib_vertexNormals();
+	mesh_methods::lib_vertexNormals();
 
 	// Compute he_per-vertex normals
-    // mesh_methods::vertexNormals(he); 
+    mesh_methods::vertexNormals(he); 
+
+	// for(int v=0; v<V.rows(); v++){
+	// 	std::cout << "N_vertices[" << v << "] = " << N_vertices.row(v).x() << ", " << N_vertices.row(v).y() << ", " << N_vertices.row(v).z() << std::endl; 
+	// 	std::cout << "lib_N_vertices[" << v << "] = " << lib_N_vertices.row(v).x() << ", " << lib_N_vertices.row(v).y() << ", " << lib_N_vertices.row(v).z() << std::endl; 
+	// 	std::cout << "he_N_vertices[" << v << "] = " << he_N_vertices.row(v).x() << ", " << he_N_vertices.row(v).y() << ", " << he_N_vertices.row(v).z() << std::endl; 
+	// 	std::cout << std::endl;
+	// }
 
 	// TASK 1.4 
 	// compute number of boundaries
-	// int B=mesh_methods::countBoundaries(he);  
-	// std::cout << "The mesh has " << B << " boundaries" << std::endl;
+	int B=mesh_methods::countBoundaries(he);  
+	std::cout << "The mesh has " << B << " boundaries" << std::endl;
 
 
 
@@ -335,14 +580,17 @@ int main(int argc, char *argv[]) {
 	
 	// TASK 2
 	// compute the gaussian curvature
-	// mesh_methods::compute_voronoi_area();
-	// mesh_methods::compute_gaussian_curvature(he); //here use the half edge structure
-	// mesh_methods::compute_lib_gaussian_curvature(); //here use the libigl face based datastructure
-	
-	
-	
-	
-	
+	mesh_methods::compute_voronoi_area();
+	mesh_methods::compute_gaussian_curvature(he); //here use the half edge structure
+	mesh_methods::compute_lib_gaussian_curvature(); //here use the libigl face based datastructure
+	MatrixXd K = MatrixXd::Zero(V.rows(), 1);
+	igl::gaussian_curvature(V, F, K);
+	for(int v=0; v<V.rows(); v++){
+		std::cout << "he_curv["  << v << "] = " << curvatures.row(v).x() << std::endl; 
+		std::cout << "lib_curv[" << v << "] = " << lib_curvatures.row(v).x() << std::endl; 
+		std::cout << "igl_curv[" << v << "] = " << K.row(v).x() << std::endl; 
+		std::cout << std::endl;
+	}
 	
 
 	/*---------------- Uncomment this part for the build of the Laplacian ----------------*/
