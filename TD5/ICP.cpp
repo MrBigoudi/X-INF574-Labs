562aa04539904e637a8c4afb041e120b804debe1
MrBigoudi <yannis.kedadry2@gmail.com>
2023-11-07 19:06:59 +0100

    lab5

---
+++ TD5/ICP.cpp
@@ -0,0 +1,19 @@
+#include "ICP.h"
+
+void nearest_neighbour(const MatrixXd &V1, const MatrixXd &V2, MatrixXd &nn_V2){
+  // return the nearest neighbour to V1 in V2 as nn_V2
+  // Complete here
+}
+
+void nearest_neighbour_point_to_plane(const MatrixXd &V1, const MatrixXd &V2, MatrixXd &nn_V2){
+  // return the nearest neighbour to V1 in V2 as nn_V2 using the point to plane algorithm
+  // Complete here
+}
+
+
+
+
+void transform(MatrixXd &V1,const MatrixXd &V2){
+  //align V1 to V2 when V1 and V2 points are in correspondance
+  // complete here
+}
---
+++ TD5/pca.cpp
@@ -0,0 +1,13 @@
+#include "pca.h"
+
+
+void k_nearest_neighbour(const MatrixXd &V1,Eigen::MatrixXi &I, int k){
+  // return the k nearest neighbour index
+  // complete here
+}
+
+
+
+void compute_normals(const MatrixXd &V1,const Eigen::MatrixXi &I, int k, MatrixXd &normals){
+  // compute the normals using PCA
+  }
